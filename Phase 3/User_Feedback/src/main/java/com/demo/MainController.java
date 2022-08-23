adaca4264e841ffa4d7b506fa301b8d6eb60a105
eshwar-095 <eshwar095@gmail.com>
2022-08-23 19:26:10 +0530

    last commit

---
+++ Phase 3/User_Feedback/src/main/java/com/demo/MainController.java
@@ -0,0 +1,27 @@
+package com.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@Autowired
+	private FeedbackDao dao;
+	
+	
+	
+	@PostMapping("/feedback")
+	public ResponseEntity<String> addFeedback(FeedbackEntity feedback) {
+		
+		int x=dao.addFedback(feedback);
+		if(x>0)
+			return new ResponseEntity<String>("Data inserted",HttpStatus.OK);
+		else
+			return new ResponseEntity<String>("Error in inserting",
+					HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
